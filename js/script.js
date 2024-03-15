69df800879501ede8b853bbb58f9ee2b664d5dd6
Brenda Vitória <brendavitoria0602@gmail.com>
2024-03-15 16:03:13 -0300

    added toggle functionality for sidebar

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.js-menu');
+const sidebar = document.querySelector('section');
+
+function clickMenu() {
+  sidebar.classList.toggle('active-sidebar');
+}
+
+menu.addEventListener('click', clickMenu);
+
